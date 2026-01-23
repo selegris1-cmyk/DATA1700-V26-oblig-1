6a1510965e418d1f8b3b478f74b262589bb0d618
selegris1-cmyk <selegris1@gmail.com>
2026-01-23 11:47:19 +0100

    Add files via upload

---
+++ scratch.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+alert("hello world")
+const value = 1.0936132983
+let number = 299
+console.log("the number is "+ number)
+function convert(a){
+return a * value
+}
+if (convert(number) >= 300){
+console.log("That is a really long golf drive")
+}
+else {console.log("this is nothing")}
